c25460aea9cf85befbb2e4295f33f59afeb2eaa5
Lucas Brasilino <brasilino_lucas@lilly.com>
2023-03-10 11:32:56 -0500

    renaming model

--- SimpleModel/train_simplemodel.py
+++ SimpleModel/train_simplemodel.py
@@ -1,10 +1,10 @@
 import torch
 from torch.cuda.nvtx import range_push,range_pop
 
-class TinyModel(torch.nn.Module):
+class SimpleModel(torch.nn.Module):
 
     def __init__(self, input_size):
-        super(TinyModel, self).__init__()
+        super(SimpleModel, self).__init__()
 
         self.linear1 = torch.nn.Linear(input_size, 200)
         self.activation = torch.nn.ReLU()
@@ -22,25 +22,23 @@ class TinyModel(torch.nn.Module):
         x = self.linear2(x)
         range_pop()
         range_push("Softmax")
-        x = self.softmax(x)
         range_pop()
         return x
 
 def main(input_size = 10*2**10):
-    tinymodel = TinyModel(input_size)
-    print('The model:')
-    print(tinymodel)
-    tinymodel = tinymodel.to('cuda:0')
+    model = SimpleModel(input_size)
+    print(f'The model:\n{model}')
+    model = model.to('cuda:0')
 
     trainset = torch.rand(10, input_size).to('cuda:0')
     target = torch.rand(10).to('cuda:0')
     criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.SGD(tinymodel.parameters(), lr=0.05)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.05)
 
     for i,sample in enumerate(trainset):
         range_push(f"MiniBatch {i}")
         range_push(f"Forward")
-        output = tinymodel(sample)
+        output = model(sample)
         range_pop()
         range_push(f"Conmpute Loss")
         loss = criterion(output,target)
